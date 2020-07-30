0328b91cd2c71ee5ace770fb9e4f9f474ae7df69
varvind7 <varvind7@gmail.com>
2020-07-29 23:21:17 -0400

    Intial config

---
+++ src/Components/MainLayout/MainLayout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return(
+        <div>Todo</div>
+    );
+};
+/*
+TODOS:
+Material UI
+Basic functionality TODO
+local state
+props transfer
+Routes
+Redux
+Thunk and Saga
+
+*/
+export default MainLayout;
